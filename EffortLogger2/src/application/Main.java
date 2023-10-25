bd1c7dc813eaa94b2ad50a33720f9d5a50291278
ErickYEnriquez <114539650+ErickYEnriquez@users.noreply.github.com>
2023-10-25 16:18:55 -0700

    Add files via upload

--- EffortLogger2/src/application/Main.java
+++ EffortLogger2/src/application/Main.java
@@ -8,14 +8,24 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
+
+import com.mongodb.BasicDBObject;
 import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.MongoException;
 import com.mongodb.ServerApi;
 import com.mongodb.ServerApiVersion;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
+import static com.mongodb.client.model.Filters.eq;
+import java.security.Timestamp;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.bson.Document;
 
 
@@ -26,17 +36,60 @@ public class Main extends Application {
     }
     
     public void start(Stage primaryStage) {
-    	String connectionString = "mongodb+srv://<username>:<password>@effortlogger.zfgzhfr.mongodb.net/?retryWrites=true&w=majority";
-        
+    	//connection string
+    	String connectionString = "mongodb+srv://eyenriqu:XmIKNbx9Oyj7YMTp@effortlogger.zfgzhfr.mongodb.net/?retryWrites=true&w=majority";
         try (MongoClient mongoClient = MongoClients.create(connectionString)) {
             try {
                 // Send a ping to confirm a successful connection
-                MongoDatabase database = mongoClient.getDatabase("Effortlogs");
-                database.runCommand(new Document("ping", 1));
-                System.out.println("Pinged your deployment. You successfully connected to MongoDB!");
+                MongoDatabase db = mongoClient.getDatabase("Effortlogs");
+                MongoCollection<Document> col = db.getCollection("logs");
+
             } catch (MongoException e) {
                 e.printStackTrace();
             }
         }
+    	
     }
+    
+    //insert into logs collection in database we have to manually pass the db
+    public void insertLog(String user, int security, String description, MongoDatabase db) {
+    	MongoCollection<Document> col = db.getCollection("logs"); 
+        Document test = new Document("user",user)
+        		.append("security-level", security)
+        		.append("Description", description)
+        		.append("TimeStamp", new java.util.Date());
+        col.insertOne(test);
+        System.out.println("Inserted document");
+    }
+    
+    //helps find every document in the database that matches a filter and data
+    //If you are looking for a user who's know is AJ then filter variable would = user 
+    //data would = AJ
+    //pass the database in the start method
+    //returns an iterable that has all the documents
+    public FindIterable<Document> findAll(String filter, String data, MongoDatabase db)
+    {
+    	MongoCollection<Document> col = db.getCollection("logs");
+    	FindIterable<Document> iterable;
+    	if(filter.equals("security-level")) {
+    		iterable = col.find(eq(filter, Integer.parseInt(data)));
+    	}
+    	else {
+    		iterable = col.find(eq(filter, data));
+    	}
+    	return iterable;
+    }
+    
+    
+    //print all logs that where found and store in an iterable
+    public void printLogs(FindIterable<Document> iterable) {
+    	MongoCursor<Document> results = iterable.iterator();
+        while(results.hasNext())
+        {
+        	System.out.println(results.next().toJson());
+       
+        }
+    }
+    
+    
 }
