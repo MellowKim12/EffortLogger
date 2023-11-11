8a4e40983df37ec5f918b28af983fecfad8176a6
ErickYEnriquez <eyenriqu@asu.edu>
2023-11-10 18:07:10 -0700

    Added Showing effort logs for user

---
+++ EffortLogger2/src/model/log.java
@@ -0,0 +1,19 @@
+package model;
+
+public class log{
+	private int logID;
+	private String Description;
+	public String getLogID() {
+		return "" + logID;
+	}
+	public void setlogID(int newlogID) {
+		logID = newlogID;
+	}
+	public String getDescription() {
+		return Description;
+	}
+	public void setDescription(String description) {
+		Description = description;
+	}	
+
+}
